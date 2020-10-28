41aaae9e4283536c1a3f90ac342d23c2ec433ac2
anpopo <65079652+anpopo@users.noreply.github.com>
2020-10-28 18:56:10 +0900

    Oracle에 Crousel을 위한 tbl_main_image 생성, 시퀀스 seq_main_image생성, 4개의 메인화면에 넣어줄 사진 insert시킴

--- MyMVCPractice/WebContent/WEB-INF/SQL/MyMVCPractice.sql
+++ MyMVCPractice/WebContent/WEB-INF/SQL/MyMVCPractice.sql
@@ -6,3 +6,28 @@
     -- Grant을(를) 성공했습니다.
 
     show user;  -- USER이(가) "MYMVCPRACTICE_USER"입니다.
+    
+    create table tbl_main_image
+    (imgno           number not null
+    ,imgfilename     varchar2(100) not null
+    ,constraint PK_tbl_main_image primary key(imgno)
+    );
+    
+    create sequence seq_main_image
+    start with 1
+    increment by 1
+    nomaxvalue
+    nominvalue
+    nocycle
+    nocache;
+    
+    insert into tbl_main_image(imgno, imgfilename) values(seq_main_image.nextval, '미샤.png');  
+    insert into tbl_main_image(imgno, imgfilename) values(seq_main_image.nextval, '원더플레이스.png'); 
+    insert into tbl_main_image(imgno, imgfilename) values(seq_main_image.nextval, '레노보.png'); 
+    insert into tbl_main_image(imgno, imgfilename) values(seq_main_image.nextval, '동원.png'); 
+    
+    commit;
+    
+    select imgno, imgfilename
+    from tbl_main_image
+    order by imgno;
--- MyMVCPractice/src/myshop/model/ProductDAO.java
+++ MyMVCPractice/src/myshop/model/ProductDAO.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.*;
 
 public class ProductDAO implements InterProductDAO {
-
+	
 	@Override
 	public List<ImageVO> imageSelectAll() throws SQLException {
 		List<ImageVO> imgList = new ArrayList<>();
